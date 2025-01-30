8966467796fb80d7917376f9e546b04172bb021d
Malvin King <malvink003@gmail.com>
2025-01-30 17:21:23 +0200

    Update and rename Readme.md to messageConfig.js

---
+++ config/messageConfig.js
@@ -0,0 +1,14 @@
+const channelInfo = {
+  'contextInfo': {
+    'forwardingScore': 0x3e7,
+    'isForwarded': true,
+    'forwardedNewsletterMessageInfo': {
+      'newsletterJid': "120363306168354073@newsletter",
+      'newsletterName': "JINWOO BOT",
+      'serverMessageId': -1
+    }
+  }
+};
+module.exports = {
+  'channelInfo': channelInfo
+};
